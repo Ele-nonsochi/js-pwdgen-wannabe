0f2240c109f587f411133d59652478d7bcc69fb3
Ele-nonsochi <elea.luzi25@gmail.com>
2021-10-07 15:21:46 +0200

    Variables and promts added

---
+++ js/script.js
@@ -0,0 +1,40 @@
+//Utente nome
+
+let nameUser="Buzz";
+
+console.log(`Richiesta ${nameUser}`);
+
+let name = prompt("What's your name?");
+
+//Utente Surname
+
+let surnameUser="Lightyear";
+
+console.log(`Richiesta ${surnameUser}`);
+
+let surname = prompt("What's your last name?");
+
+//Favourite Color
+
+let colorUser="Green";
+
+console.log(`Richiesta ${colorUser}`);
+
+let color = prompt("What's your favorite color?");
+
+//Alert
+
+alert (`La tua password è ${nameUser}${surnameUser}${colorUser}21`)
+
+
+
+
+
+
+
+
+
+
+
+
+
